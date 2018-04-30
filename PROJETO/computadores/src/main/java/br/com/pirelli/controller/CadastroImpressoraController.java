fb06c04e29bf55e75e77fb4d63be0cbf938172ab
Vinicius <vinicius_cok@hotmail.com>
2018-04-30 14:31:30 -0300

    30-04-2018-Trabalho

--- PROJETO/computadores/src/main/java/br/com/pirelli/controller/CadastroImpressoraController.java
+++ PROJETO/computadores/src/main/java/br/com/pirelli/controller/CadastroImpressoraController.java
@@ -1,10 +1,15 @@
 package br.com.pirelli.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import br.com.pirelli.model.Impressora;
 import br.com.pirelli.model.CategoriaImpressora;
@@ -13,8 +18,11 @@ import br.com.pirelli.model.TipoImpressora;
 import br.com.pirelli.repository.Filiais;
 import br.com.pirelli.repository.Marcas;
 import br.com.pirelli.repository.Modelos;
+import br.com.pirelli.repository.Setores;
 import br.com.pirelli.repository.Toners;
 import br.com.pirelli.service.CadastroImpressoraService;
+import br.com.pirelli.service.exception.ComputadorJaCadastradoException;
+import br.com.pirelli.service.exception.ImpressoraJaExisteException;
 
 @Controller
 @RequestMapping("/impressoras")
@@ -35,6 +43,9 @@ public class CadastroImpressoraController
 	@Autowired
 	private Toners toners;
 	
+	@Autowired
+	private Setores setores;
+	
 	@GetMapping("/novo")
 	public ModelAndView novo(Impressora impressora)
 	{
@@ -46,8 +57,31 @@ public class CadastroImpressoraController
 		mv.addObject("statusImpressora", Status.values());
 		mv.addObject("tipoImpressoras", TipoImpressora.values());
 		mv.addObject("categorias", CategoriaImpressora.values());
+		mv.addObject("setores", setores.findAll());
 		
 		return mv;
+	}
+	
+	@PostMapping("/novo")
+	public ModelAndView salvar(@Valid Impressora impressora, BindingResult result, RedirectAttributes attributes)
+	{
+		if(result.hasErrors())
+		{
+			return novo(impressora);
+		}
+		
+		try
+		{
+			cadastroImpressoraService.salvar(impressora);
+		}catch(ImpressoraJaExisteException e)
+		{
+			result.rejectValue("enderecoIP", e.getMessage(), e.getMessage());
+			return novo(impressora);
+		}
+		
+		attributes.addFlashAttribute("mensagem", "Impressora cadastrada com sucesso");
+		
+		return new ModelAndView("redirect:/impressoras/novo");
 	}
 	
 	
--- PROJETO/computadores/src/main/java/br/com/pirelli/filter/ComputadorFilter.java
+++ PROJETO/computadores/src/main/java/br/com/pirelli/filter/ComputadorFilter.java
@@ -1,9 +1,21 @@
 package br.com.pirelli.filter;
 
+import br.com.pirelli.model.Impressora;
+import br.com.pirelli.model.Modelo;
+import br.com.pirelli.model.So;
+import br.com.pirelli.model.Status;
+import br.com.pirelli.model.TipoComputador;
+
 public class ComputadorFilter 
 {
 	private Long codigo;
 	private String nome;
+	private TipoComputador tipoComputador;
+	private Modelo modelo;
+	private So sistemaOperacional;
+	private String code;
+	private Impressora impressora;
+	private Status status;
 
 	
 	
@@ -21,6 +33,54 @@ public class ComputadorFilter
 
 	public void setNome(String nome) {
 		this.nome = nome;
+	}
+
+	public TipoComputador getTipoComputador() {
+		return tipoComputador;
+	}
+
+	public void setTipoComputador(TipoComputador tipoComputador) {
+		this.tipoComputador = tipoComputador;
+	}
+
+	public Modelo getModelo() {
+		return modelo;
+	}
+
+	public void setModelo(Modelo modelo) {
+		this.modelo = modelo;
+	}
+
+	public So getSistemaOperacional() {
+		return sistemaOperacional;
+	}
+
+	public void setSistemaOperacional(So sistemaOperacional) {
+		this.sistemaOperacional = sistemaOperacional;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public Impressora getImpressora() {
+		return impressora;
+	}
+
+	public void setImpressora(Impressora impressora) {
+		this.impressora = impressora;
+	}
+
+	public Status getStatus() {
+		return status;
+	}
+
+	public void setStatus(Status status) {
+		this.status = status;
 	}
 	
 	
